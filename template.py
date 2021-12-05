d1246ba80ca88cf91d2d566b9c4ec31f7a103372
haydendaly <hcd36@cornell.edu>
2021-12-05 09:44:22 -0500

    adds template

---
+++ template.py
@@ -0,0 +1,21 @@
+def format_input(raw):
+    rows = raw.split("\n")
+    return rows
+
+
+def part1(depths):
+    return rows
+
+
+def part2(rows):
+    return rows
+
+
+if __name__ == "__main__":
+    raw = open("data/x.txt", "r").read()
+    rows = format_input(raw)
+    print(part1(rows))
+
+    raw = open("data/x.txt", "r").read()
+    rows = format_input(raw)
+    print(part2(rows))
